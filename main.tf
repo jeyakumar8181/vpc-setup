6d5302871c28f294152fc524b081382d688590fa
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-09-08 22:37:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
+
 
 data "aws_availability_zones" "available" {
 }
